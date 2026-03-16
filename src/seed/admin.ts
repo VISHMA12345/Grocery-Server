5678b23fadf7e32f236fb615513465bfcb7266bb
VISHMA12345 <vishma@email.com>
2026-03-16 21:46:29 +0530

    added currections

--- src/seed/admin.ts
+++ src/seed/admin.ts
@@ -1,27 +1,27 @@
 import { AppDataSource } from "../data-source";
-import { Admin } from "../entity/Users";
+// import { Admin } from "../entity/Users";
 import bcrypt from "bcryptjs";
 
 export async function seedDefaultAdmin() {
-  const adminRepo = AppDataSource.getMongoRepository(Admin);
+  // const adminRepo = AppDataSource.getMongoRepository(Admin);
 
-  const count = await adminRepo.countDocuments({ isDelete: 0 });
+  // const count = await adminRepo.countDocuments({ isDelete: 0 });
 
-  if (count > 0) {
-    return;
-  }
+  // if (count > 0) {
+  //   return;
+  // }
 
-  const defaultAdmin = new Admin();
-  defaultAdmin.name = "Mars Solutions";
-  defaultAdmin.email = "admin@mars.in";
-  defaultAdmin.companyName = "Mars Solutions";
-  defaultAdmin.phoneNumber = "9361570434";
-  defaultAdmin.pin = await bcrypt.hash("1234", 10);
-  defaultAdmin.role = "ADMIN";
-  defaultAdmin.isActive = 1;
-  defaultAdmin.isDelete = 0;
+  // const defaultAdmin = new Admin();
+  // defaultAdmin.name = "Mars Solutions";
+  // defaultAdmin.email = "admin@mars.in";
+  // defaultAdmin.companyName = "Mars Solutions";
+  // defaultAdmin.phoneNumber = "9361570434";
+  // defaultAdmin.pin = await bcrypt.hash("1234", 10);
+  // defaultAdmin.role = "ADMIN";
+  // defaultAdmin.isActive = 1;
+  // defaultAdmin.isDelete = 0;
 
-  await adminRepo.save(defaultAdmin);
+  // await adminRepo.save(defaultAdmin);
 
   console.log("🌟 Default Admin seeded successfully");
 }
